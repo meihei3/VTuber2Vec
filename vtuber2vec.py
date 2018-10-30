d6d5a561d48721cc6abe5c0d88064acfa4079f85
yameholo <ame.aroid.xz5@gmail.com>
2018-10-31 05:33:42 +0900

    Update model training params

--- vtuber2vec.py
+++ vtuber2vec.py
@@ -1,22 +1,26 @@
 from gensim.models.doc2vec import Doc2Vec
 from gensim.models.doc2vec import TaggedDocument
 
+from tqdm import tqdm
+
 import pandas as pd
 
 from utils import get_title_usable
 
 
 def main():
-    vtuber = pd.read_csv("VTuberData/VTuber_list.csv", index_col=0)
-    training = [TaggedDocument(words=get_title_usable(i), tags=[name]) for i, name in zip(vtuber.index, vtuber.name)]
-    model = Doc2Vec(documents=training, dm=0, size=100, window=5, alpha=0.01,
-                    min_alpha=0.001, min_count=1)
-    print('\nStart Training')
-    for epoch in range(10):
-        print('Epoch: {}'.format(epoch + 1))
+    vtuber = pd.read_csv("VTuberData/VTuber_list.csv", index_col=0).dropna().reset_index(drop=True)
+    print("pre processing vtuber titles")
+    training = [
+        TaggedDocument(words=get_title_usable(i), tags=[name]) for i, name in zip(tqdm(vtuber.index), vtuber.name)
+    ]
+    print("\ntraining")
+    model = Doc2Vec(documents=training, dm=0, window=3, alpha=0.013, min_alpha=0.013, min_count=1)
+    for _ in tqdm(range(15)):
         model.train(training, total_examples=model.corpus_count, epochs=model.iter)
-        model.alpha -= 0.001
-    model.save('vtuber2vec_2.model')
+        model.alpha -= 0.0012
+        model.min_alpha = model.alpha
+    model.save('vtuber2vec.model')
 
 
 if __name__ == '__main__':
